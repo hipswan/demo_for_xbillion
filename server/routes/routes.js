9c2cf3bf53e99c41fb6aac11d5dccbc7f127803a
Atul Singh <46633779+hipswan@users.noreply.github.com>
2020-11-23 01:44:52 +0530

    postgres sql

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,7 +6,7 @@ var db = pgp(
   "postgres://mlyllqsbannsmo:242faf070b44b39ad59398e86eaf77ef181ad311286f3ae3cc1c39526ddaba48@ec2-54-163-47-62.compute-1.amazonaws.com:5432/d59bbr1se23804"
 );
 router.get("/", function (req, res) {
-  db.any('SELECT * FROM fullstack."video"')
+  db.any('SELECT $1:json FROM fullstack."video"', ["*"])
     .then((data) => {
       data.map((row, index, data) => {
         console.log(row);
