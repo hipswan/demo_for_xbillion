2d3729cf74fd4d50441c1f40bee3287322ff5d03
Atul Singh <46633779+hipswan@users.noreply.github.com>
2020-11-23 00:11:23 +0530

    postgres update

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,16 +5,17 @@ var pgp = require("pg-promise")(/* options */);
 var db = pgp(
   "postgres://mlyllqsbannsmo:242faf070b44b39ad59398e86eaf77ef181ad311286f3ae3cc1c39526ddaba48@ec2-54-163-47-62.compute-1.amazonaws.com:5432/d59bbr1se23804"
 );
-
 router.get("/", function (req, res) {
   db.manyOrNone("SELECT * FROM fullstack.video")
     .then(function (data) {
-      console.log("DATA:", data);
+      console.log("DATA:", data.value);
+      res.send(data.value);
     })
     .catch(function (error) {
       console.log("ERROR:", error);
     });
 });
+
 // router.get("/", function (req, res) {
 //   res.render("index");
 // });
