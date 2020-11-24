d4e701552a2acc0028a238927ed3e6313424f200
Atul Singh <atulsingh158@gmail.com>
2020-11-24 20:38:23 +0530

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,14 +9,6 @@ router.get("/id/:id", function (req, res) {
   const where = pgp.as.format("WHERE id = $1", [req.params.id]);
   db.any('SELECT * FROM fullstack."video" $1:raw', where)
     .then((data) => {
-      // data.map((row, index, data) => {
-      //   console.log(row);
-      //   console.log(index);
-      //   console.log(data);
-      //   console.log(data.value);
-      // });
-      // console.log("DATA:", data.value);
-
       res.send(data);
     })
     .catch(function (error) {
@@ -28,59 +20,4 @@ router.get("*", function (req, res) {
   response.write("404 Not found");
   response.end();
 });
-// router.get("/", function (req, res) {
-//   res.render("index");
-// });
-// router.route("/insert").post(function (req, res) {
-//   var expense = new Expense();
-//   expense.description = req.body.desc;
-//   expense.amount = req.body.amount;
-//   expense.month = req.body.month;
-//   expense.year = req.body.year;
-//   expense.save(function (err) {
-//     if (err) res.send(err);
-//     res.send("Expense successfully added!");
-//   });
-// });
-// router.route("/update").post(function (req, res) {
-//   console.log(req.body._id);
-//   const doc = {
-//     description: req.body.description,
-//     amount: req.body.amount,
-//     month: req.body.month,
-//     year: req.body.year,
-//   };
-//   Expense.update({ _id: req.body._id }, { $set: doc }, function (err, result) {
-//     if (err) res.send(err);
-//     res.send("Expense successfully updated!");
-//   });
-// });
-
-// router.get("/delete", function (req, res) {
-//   var id = req.query.id;
-//   Expense.find({ _id: id })
-//     .remove()
-//     .exec(function (err, expense) {
-//       if (err) res.send(err);
-//       res.send("Expense successfully deleted!");
-//     });
-// });
-// router.get("/getAll", function (req, res) {
-//   var monthRec = req.query.month;
-//   var yearRec = req.query.year;
-//   if (monthRec && monthRec !== "All") {
-//     Expense.find(
-//       { $and: [{ month: monthRec }, { year: yearRec }] },
-//       function (err, expenses) {
-//         if (err) res.send(err);
-//         res.json(expenses);
-//       }
-//     );
-//   } else {
-//     Expense.find({ year: yearRec }, function (err, expenses) {
-//       if (err) res.send(err);
-//       res.json(expenses);
-//     });
-//   }
-// });
 module.exports = router;
